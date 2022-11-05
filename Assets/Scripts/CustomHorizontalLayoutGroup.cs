50f3399ef2824ed5d2364f7db4b02c1776dba198
calcruikshank <calcruikshank@gmail.com>
2022-11-04 22:53:37 -0600

    custom horizontal layout group

--- Assets/Scripts/CustomHorizontalLayoutGroup.cs
+++ Assets/Scripts/CustomHorizontalLayoutGroup.cs
@@ -7,26 +7,24 @@ public class CustomHorizontalLayoutGroup : MonoBehaviour
     float spacing;
 
     int previousNumberOfChildren;
+    float startingOffset;
     private void Start()
     {
         CalculateCorrrectTransformsOfChildren();
     }
     private void Update()
     {
-        if (transform.childCount != previousNumberOfChildren)
-        {
-            CalculateCorrrectTransformsOfChildren();
-            previousNumberOfChildren = transform.childCount;
-        }
+        CalculateCorrrectTransformsOfChildren();
     }
     public void CalculateCorrrectTransformsOfChildren()
     {
         for (int i = 0; i < transform.childCount; i++)
         {
-            float widthOfChild = transform.GetChild(i).transform.GetComponent<RectTransform>().sizeDelta.x;
-            spacing = widthOfChild * .2f;
-            transform.GetChild(i).localPosition = new Vector3( (i * widthOfChild) + spacing * i, transform.GetChild(i).localPosition.y, transform.GetChild(i).localPosition.z);
-            Debug.Log(i);
+            float widthOfChild = transform.GetChild(i).transform.GetComponent<RectTransform>().sizeDelta.x * transform.GetChild(i).transform.GetComponent<RectTransform>().localScale.x;
+            startingOffset = ((widthOfChild / 2 * transform.childCount) - widthOfChild / 2);
+            spacing = widthOfChild * .3f;
+            startingOffset = startingOffset +  widthOfChild / 2 + spacing;
+            transform.GetChild(i).localPosition = new Vector3( ((i * widthOfChild) + spacing * i) - startingOffset, transform.GetChild(i).localPosition.y, transform.GetChild(i).localPosition.z);
         }
     }
 }
