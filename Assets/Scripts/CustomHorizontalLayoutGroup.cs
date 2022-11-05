d6f065c74425739d274d7a4d5c48e84472401571
calcruikshank <calcruikshank@gmail.com>
2022-11-04 23:58:12 -0600

    correct order

--- Assets/Scripts/CustomHorizontalLayoutGroup.cs
+++ Assets/Scripts/CustomHorizontalLayoutGroup.cs
@@ -6,25 +6,42 @@ public class CustomHorizontalLayoutGroup : MonoBehaviour
 {
     float spacing;
 
-    int previousNumberOfChildren;
+    int numberOfChildren;
     float startingOffset;
-    private void Start()
-    {
-        CalculateCorrrectTransformsOfChildren();
-    }
+
+    int middleNumber = 0;
     private void Update()
     {
         CalculateCorrrectTransformsOfChildren();
     }
     public void CalculateCorrrectTransformsOfChildren()
     {
-        for (int i = 0; i < transform.childCount; i++)
+        numberOfChildren = transform.childCount;
+        for (int i = 0; i < numberOfChildren; i++)
         {
+            if (numberOfChildren % 2 == 0)
+            {
+                middleNumber = numberOfChildren / 2;
+            }
+            if (numberOfChildren % 2 != 0)
+            {
+                middleNumber = (numberOfChildren - 1) / 2;
+            }
+            int distanceFromMiddleNumber = i - middleNumber;
+            Debug.Log(distanceFromMiddleNumber);
             float widthOfChild = transform.GetChild(i).transform.GetComponent<RectTransform>().sizeDelta.x * transform.GetChild(i).transform.GetComponent<RectTransform>().localScale.x;
-            startingOffset = ((widthOfChild / 2 * transform.childCount) - widthOfChild / 2);
-            spacing = widthOfChild * .3f;
-            startingOffset = startingOffset +  widthOfChild / 2 + spacing;
-            transform.GetChild(i).localPosition = new Vector3( ((i * widthOfChild) + spacing * i) - startingOffset, transform.GetChild(i).localPosition.y, transform.GetChild(i).localPosition.z);
+            
+            spacing = widthOfChild * .3f; 
+            if (numberOfChildren % 2 != 0)
+            {
+                startingOffset = (widthOfChild + spacing) * distanceFromMiddleNumber;
+            }
+            if (numberOfChildren % 2 == 0)
+            {
+                startingOffset = ((widthOfChild + spacing) * distanceFromMiddleNumber) + (widthOfChild / 2);
+            }
+            transform.GetChild(i).localPosition = new Vector3( startingOffset, transform.GetChild(i).localPosition.y, transform.GetChild(i).localPosition.z);
+            transform.GetChild(i).localEulerAngles = new Vector3();
         }
     }
 }
