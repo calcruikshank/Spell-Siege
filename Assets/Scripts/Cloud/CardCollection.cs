0bde0b70553c7ef763dfdace4c8ad7d4397e420a
calcruikshank <calcruikshank@gmail.com>
2023-01-19 05:52:14 -0700

    moved card data to main menu

--- Assets/Scripts/Cloud/CardCollection.cs
+++ Assets/Scripts/Cloud/CardCollection.cs
@@ -9,10 +9,6 @@ using System.Linq;
 public class CardCollection : MonoBehaviour
 {
     public static CardCollection singleton;
-    CardsCollectedForPlayer loadedCollection;
-
-    List<Deck> decks = new List<Deck>();
-
     Deck currentSelectedDeck;
 
     private void Awake()
@@ -25,221 +21,14 @@ public class CardCollection : MonoBehaviour
     }
     private void Start()
     {
-        LoadAllDecks();
-        LoadSomeData();
+        LoadCardCollection(CardCollectionData.singleton.loadedCollection);
     }
 
 
-    public void LoadAllDecks()
-    {
-        LoadDeck0FromCloudSave();
-        LoadDeck1FromCloudSave();
-        LoadDeck2FromCloudSave();
-        LoadDeck3FromCloudSave();
-        LoadDeck4FromCloudSave();
-        LoadDeck5FromCloudSave();
-    }
-    public async void SaveDecklists()
-    {
-        SaveDeckList0();
-        SaveDeckList1();
-        SaveDeckList2();
-        SaveDeckList3();
-        SaveDeckList4();
-        SaveDeckList5();
-    }
-
-    #region decklistsCloudSave
-    public async void SaveInitialDecklist0()
-    {
-        Deck deck0 = new Deck();
-        var data = new Dictionary<string, object> { { "Deck0", deck0 } };
-        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
-        LoadDeck0FromCloudSave();
-    }
-
-    private async void LoadDeck0FromCloudSave()
-    {
-        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "Deck0" });
-        if (savedData.Count < 1)
-        {
-            SaveInitialDecklist0();
-            return;
-        }
-        decks.Add(JsonUtility.FromJson<Deck>(savedData["Deck0"]));
-    }
-    private async void SaveDeckList0()
-    {
-        var data = new Dictionary<string, object> { { "Deck0", decks[0] } };
-        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
-    }
-    public async void SaveInitialDecklist1()
-    {
-        Deck deck1 = new Deck();
-        var data = new Dictionary<string, object> { { "Deck1", deck1 } };
-        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
-        LoadDeck1FromCloudSave();
-    }
-
-    private async void LoadDeck1FromCloudSave()
-    {
-        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "Deck1" });
-        if (savedData.Count < 1)
-        {
-            SaveInitialDecklist1();
-            return;
-        }
-        decks.Add(JsonUtility.FromJson<Deck>(savedData["Deck1"]));
-    }
-    private async void SaveDeckList1()
-    {
-        var data = new Dictionary<string, object> { { "Deck1", decks[1] } };
-        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
-    }
-
-
-
-
-    public async void SaveInitialDecklist2()
-    {
-        Deck deck2 = new Deck();
-        var data = new Dictionary<string, object> { { "Deck2", deck2 } };
-        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
-        LoadDeck2FromCloudSave();
-    }
-
-    private async void LoadDeck2FromCloudSave()
-    {
-        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "Deck2" });
-        if (savedData.Count < 1)
-        {
-            SaveInitialDecklist2();
-            return;
-        }
-        decks.Add(JsonUtility.FromJson<Deck>(savedData["Deck2"]));
-    }
-    private async void SaveDeckList2()
-    {
-        var data = new Dictionary<string, object> { { "Deck2", decks[2] } };
-        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
-    }
-
-
-
-    public async void SaveInitialDecklist3()
-    {
-        Deck deck3 = new Deck();
-        var data = new Dictionary<string, object> { { "Deck3", deck3 } };
-        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
-        LoadDeck3FromCloudSave();
-    }
-
-    private async void LoadDeck3FromCloudSave()
-    {
-        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "Deck3" });
-        if (savedData.Count < 1)
-        {
-            SaveInitialDecklist3();
-            return;
-        }
-        decks.Add(JsonUtility.FromJson<Deck>(savedData["Deck3"]));
-    }
-    private async void SaveDeckList3()
-    {
-        var data = new Dictionary<string, object> { { "Deck3", decks[3] } };
-        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
-    }
-
-
-
-
-    public async void SaveInitialDecklist4()
-    {
-        Deck deck4 = new Deck();
-        var data = new Dictionary<string, object> { { "Deck4", deck4 } };
-        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
-        LoadDeck4FromCloudSave();
-    }
-
-    private async void LoadDeck4FromCloudSave()
-    {
-        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "Deck4" });
-        if (savedData.Count < 1)
-        {
-            SaveInitialDecklist4();
-            return;
-        }
-        decks.Add(JsonUtility.FromJson<Deck>(savedData["Deck4"]));
-    }
-    private async void SaveDeckList4()
-    {
-        var data = new Dictionary<string, object> { { "Deck4", decks[4] } };
-        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
-    }
-
-
-
-    public async void SaveInitialDecklist5()
-    {
-        Deck deck5 = new Deck();
-        var data = new Dictionary<string, object> { { "Deck5", deck5 } };
-        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
-        LoadDeck5FromCloudSave();
-    }
-
-    private async void LoadDeck5FromCloudSave()
-    {
-        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "Deck5" });
-        if (savedData.Count < 1)
-        {
-            SaveInitialDecklist5();
-            return;
-        }
-        decks.Add(JsonUtility.FromJson<Deck>(savedData["Deck5"]));
-    }
-    private async void SaveDeckList5()
-    {
-        var data = new Dictionary<string, object> { { "Deck5", decks[5] } };
-        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
-    }
-
-
-
-    #endregion
-
-
-
-
-
-
-
-    public async void SaveInitialCardsCollected()
-    {
-        CardsCollectedForPlayer baseCardsCollectedForPlayer = new CardsCollectedForPlayer();
-        var data = new Dictionary<string, object> { { "CardsCollected", baseCardsCollectedForPlayer } };
-        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
-        LoadSomeData();
-    }
-    public async void LoadSomeData()
-    {
-        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "CardsCollected" });
-
-        if (savedData.Count < 1)
-        {
-            SaveInitialCardsCollected();
-            Debug.Log("Loading card collection for the first time");
-            return;
-        }
-        loadedCollection = JsonUtility.FromJson<CardsCollectedForPlayer>(savedData["CardsCollected"]);
-
-        LoadCardCollection(loadedCollection);
-
-    }
 
 
     [SerializeField] Transform cardHolder;
 
-    [SerializeField] Transform angryTurtleButton;
 
     public int lastInstantiatedInt = 0;
 
@@ -302,9 +91,9 @@ public class CardCollection : MonoBehaviour
 
     public async void TempOpenPack()
     {
-        if (loadedCollection == null)
+        if (CardCollectionData.singleton.loadedCollection == null)
         {
-            LoadSomeData();
+            CardCollectionData.singleton.LoadSomeData();
         }
         List<int> cardsOpened = new List<int>();
         for (int i = 0; i < 5; i++)
@@ -313,16 +102,16 @@ public class CardCollection : MonoBehaviour
         }
         foreach (int i in cardsOpened)
         {
-            loadedCollection.cardsCollected.Add(i);
+            CardCollectionData.singleton.loadedCollection.cardsCollected.Add(i);
         }
-        var data = new Dictionary<string, object> { { "CardsCollected", loadedCollection } };
+        var data = new Dictionary<string, object> { { "CardsCollected", CardCollectionData.singleton.loadedCollection } };
         await CloudSaveService.Instance.Data.ForceSaveAsync(data);
-        LoadCardCollection(loadedCollection);
+        LoadCardCollection(CardCollectionData.singleton.loadedCollection);
     }
 
     public void ChooseDeck(int deckChosen)
     {
-        currentSelectedDeck = decks[deckChosen];
+        currentSelectedDeck = CardCollectionData.singleton.decks[deckChosen];
         LoadPanelWithCurrentlySelectedDeck(currentSelectedDeck);
     }
 
@@ -356,7 +145,7 @@ public class CardCollection : MonoBehaviour
         loadedDeckPanel.gameObject.SetActive(false);
         deckSelectionPanel.gameObject.SetActive(true);
 
-        SaveDecklists();
+        CardCollectionData.singleton.SaveDecklists();
     }
 
 
---
+++ Assets/Scripts/Cloud/CardCollectionData.cs
@@ -0,0 +1,235 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Services.CloudSave;
+using UnityEngine;
+
+public class CardCollectionData : MonoBehaviour
+{
+    public static CardCollectionData singleton;
+    // Start is called before the first frame update
+    
+    public List<Deck> decks = new List<Deck>();
+
+    private void Awake()
+    {
+        if (singleton != null)
+        {
+            Destroy(this);
+        }
+        DontDestroyOnLoad(this);
+        singleton = this;
+    }
+    void Start()
+    {
+        LoadAllDecks();
+        LoadSomeData();
+    }
+
+    public void LoadAllDecks()
+    {
+        LoadDeck0FromCloudSave();
+        LoadDeck1FromCloudSave();
+        LoadDeck2FromCloudSave();
+        LoadDeck3FromCloudSave();
+        LoadDeck4FromCloudSave();
+        LoadDeck5FromCloudSave();
+    }
+    public void SaveDecklists()
+    {
+        SaveDeckList0();
+        SaveDeckList1();
+        SaveDeckList2();
+        SaveDeckList3();
+        SaveDeckList4();
+        SaveDeckList5();
+    }
+
+    #region decklistsCloudSave
+    public async void SaveInitialDecklist0()
+    {
+        Deck deck0 = new Deck();
+        var data = new Dictionary<string, object> { { "Deck0", deck0 } };
+        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
+        LoadDeck0FromCloudSave();
+    }
+
+    private async void LoadDeck0FromCloudSave()
+    {
+        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "Deck0" });
+        if (savedData.Count < 1)
+        {
+            SaveInitialDecklist0();
+            return;
+        }
+        decks.Add(JsonUtility.FromJson<Deck>(savedData["Deck0"]));
+    }
+    private async void SaveDeckList0()
+    {
+        var data = new Dictionary<string, object> { { "Deck0", decks[0] } };
+        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
+    }
+    public async void SaveInitialDecklist1()
+    {
+        Deck deck1 = new Deck();
+        var data = new Dictionary<string, object> { { "Deck1", deck1 } };
+        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
+        LoadDeck1FromCloudSave();
+    }
+
+    private async void LoadDeck1FromCloudSave()
+    {
+        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "Deck1" });
+        if (savedData.Count < 1)
+        {
+            SaveInitialDecklist1();
+            return;
+        }
+        decks.Add(JsonUtility.FromJson<Deck>(savedData["Deck1"]));
+    }
+    private async void SaveDeckList1()
+    {
+        var data = new Dictionary<string, object> { { "Deck1", decks[1] } };
+        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
+    }
+
+
+
+
+    public async void SaveInitialDecklist2()
+    {
+        Deck deck2 = new Deck();
+        var data = new Dictionary<string, object> { { "Deck2", deck2 } };
+        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
+        LoadDeck2FromCloudSave();
+    }
+
+    private async void LoadDeck2FromCloudSave()
+    {
+        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "Deck2" });
+        if (savedData.Count < 1)
+        {
+            SaveInitialDecklist2();
+            return;
+        }
+        decks.Add(JsonUtility.FromJson<Deck>(savedData["Deck2"]));
+    }
+    private async void SaveDeckList2()
+    {
+        var data = new Dictionary<string, object> { { "Deck2", decks[2] } };
+        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
+    }
+
+
+
+    public async void SaveInitialDecklist3()
+    {
+        Deck deck3 = new Deck();
+        var data = new Dictionary<string, object> { { "Deck3", deck3 } };
+        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
+        LoadDeck3FromCloudSave();
+    }
+
+    private async void LoadDeck3FromCloudSave()
+    {
+        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "Deck3" });
+        if (savedData.Count < 1)
+        {
+            SaveInitialDecklist3();
+            return;
+        }
+        decks.Add(JsonUtility.FromJson<Deck>(savedData["Deck3"]));
+    }
+    private async void SaveDeckList3()
+    {
+        var data = new Dictionary<string, object> { { "Deck3", decks[3] } };
+        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
+    }
+
+
+
+
+    public async void SaveInitialDecklist4()
+    {
+        Deck deck4 = new Deck();
+        var data = new Dictionary<string, object> { { "Deck4", deck4 } };
+        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
+        LoadDeck4FromCloudSave();
+    }
+
+    private async void LoadDeck4FromCloudSave()
+    {
+        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "Deck4" });
+        if (savedData.Count < 1)
+        {
+            SaveInitialDecklist4();
+            return;
+        }
+        decks.Add(JsonUtility.FromJson<Deck>(savedData["Deck4"]));
+    }
+    private async void SaveDeckList4()
+    {
+        var data = new Dictionary<string, object> { { "Deck4", decks[4] } };
+        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
+    }
+
+
+
+    public async void SaveInitialDecklist5()
+    {
+        Deck deck5 = new Deck();
+        var data = new Dictionary<string, object> { { "Deck5", deck5 } };
+        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
+        LoadDeck5FromCloudSave();
+    }
+
+    private async void LoadDeck5FromCloudSave()
+    {
+        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "Deck5" });
+        if (savedData.Count < 1)
+        {
+            SaveInitialDecklist5();
+            return;
+        }
+        decks.Add(JsonUtility.FromJson<Deck>(savedData["Deck5"]));
+    }
+    private async void SaveDeckList5()
+    {
+        var data = new Dictionary<string, object> { { "Deck5", decks[5] } };
+        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
+    }
+
+
+
+    #endregion
+
+
+
+
+
+
+
+    public async void SaveInitialCardsCollected()
+    {
+        CardsCollectedForPlayer baseCardsCollectedForPlayer = new CardsCollectedForPlayer();
+        var data = new Dictionary<string, object> { { "CardsCollected", baseCardsCollectedForPlayer } };
+        await CloudSaveService.Instance.Data.ForceSaveAsync(data);
+        LoadSomeData();
+    }
+
+
+    public CardsCollectedForPlayer loadedCollection;
+    public async void LoadSomeData()
+    {
+        Dictionary<string, string> savedData = await CloudSaveService.Instance.Data.LoadAsync(new HashSet<string> { "CardsCollected" });
+
+        if (savedData.Count < 1)
+        {
+            SaveInitialCardsCollected();
+            Debug.Log("Loading card collection for the first time");
+            return;
+        }
+        loadedCollection = JsonUtility.FromJson<CardsCollectedForPlayer>(savedData["CardsCollected"]);
+
+
+    }
+}
