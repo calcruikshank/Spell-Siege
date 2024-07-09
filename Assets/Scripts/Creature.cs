d48e658ae2f514330067736a750afd77b4ee24f3
calcruikshank <59811572+calcruikshank@users.noreply.github.com>
2024-07-08 17:11:18 -0700

    juio

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -161,6 +161,7 @@ public class Creature : MonoBehaviour
                 CheckForCreaturesWithinRange();
                 HandleAbilityRate();
                 DrawLine();
+                ChooseTarget();
                 //HandleFriendlyCreaturesList();
                 //HandleAttack();
                 break;
